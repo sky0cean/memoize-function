62b8fbbb1b12f08f5f9ac9134d54c911cae55e21
sky0cean <rinrinrinko@yahoo.com>
2018-06-24 01:43:15 -0400

    moving file

---
+++ app.js
@@ -0,0 +1,40 @@
+// define memoize function
+function memoize(n) {
+  cache={};
+    return function() {
+        let values = JSON.stringify(arguments);
+    // in cashe or not?
+    if (cache[values]){
+       // Yes, then return it
+    return cache[values];
+  }
+        //No, then cashe the results of its call 
+     else {
+      let value = n.apply(this, arguments);
+      cache[values] = value;
+      return value;
+    }
+     };
+}
+
+
+let foo = function (x) {
+console.log("calculating");
+return x * x;
+}
+
+
+let memoizedFoo = memoize(foo);
+
+console.log(memoizedFoo(5));
+// calculating!
+// 25
+
+//returns the results from cache
+// 25
+console.log(memoizedFoo(5));
+
+
+// 100 (notice how 'calculating!' is not printed this time)
+
+console.log(memoizedFoo(10));
